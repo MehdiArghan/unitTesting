85090a2ed57c7995e93ce853fbf85a238cfdb89e
mehdi <mehdiarghan@gmail.com.git/>
2023-10-13 12:17:46 +0330

    The main class methods were written

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        Deduction deductionOne = new Deduction(1, 2);
+        Deduction deductionTwo = new Deduction(3, 4);
+        Deduction deduction = deductionOne.division(deductionTwo);
+        deduction.print();
+        System.out.println(deduction.asFraction(deduction.getNumerator(), deduction.getDenominator()));
     }
 }
